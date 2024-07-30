48a49b89a4d253cd404cf91ebbed0f140684f74d
Priya Kshirsagar <144653180+PiuV2115@users.noreply.github.com>
2024-07-30 20:59:54 +0530

    Update and rename Linear_Search.cpp to Ass1_Linear_Search.cpp

---
+++ DSPS Practical/Ass1_Linear_Search.cpp
@@ -0,0 +1,71 @@
+//Linear Search
+
+#include <iostream>
+using namespace std;
+class linear
+{
+    public:
+    int i,j,a[10],temp=0,flag=0,size,key,press;
+    
+    void sort()
+    {
+        cout<<"Enter Your Size of Array:  ";
+        cin>>size;
+        cout<<"\nEnter Array Elements:  ";
+        for(i=0;i<size;i++)
+        {
+            cin>>a[i];
+        }
+        for(i=1;i<size;i++)
+        {
+            for(j=0;j<i;j++)
+            {
+                if(a[j]>a[i])
+                {
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+        cout<<"\nSorted Element are : ";
+        for(i=0;i<size;i++)
+        {
+            cout<<""<<a[i]<<"\t";
+        }
+    }
+    
+    void search()
+    {
+        do{
+        cout<<"\n\nEnter Key Element you want search:  ";
+        cin>>key;
+        for(i=0;i<size;i++)
+        {
+            if(key==a[i])
+            {
+                flag=1;
+                break;
+            }
+        }
+        if(flag!=1)
+        {
+         cout<<"\n\nElement "<<key<<" is NOT Found " ;   
+        }
+        else
+        {
+            cout<<"\n\nElement "<<key<<" is Found at "<<i<<" Position" ; 
+        }
+        cout<<"\n\nDo You want to search Again , Press 1 : ";
+        cin>>press;
+        }while(press==1);
+    }
+};
+int main()
+{
+   linear p1;
+   p1.sort();
+   p1.search();
+
+    return 0;
+}
